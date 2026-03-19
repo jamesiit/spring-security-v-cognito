bc39052bea7f9981aa56e5e5e84f74da5bf8aff7
James Motha <james.20232826@iit.ac.lk>
2026-03-19 11:34:34 +0530

    Token generated
    - create a method that dynamically generates a secret key
    
    - made the constructor of JWTService such that the generateSecretKey method runs on startup
    
    - getKey method added to convert the String (the generated secretKey on startup) back to a byte array, then return it as a Key object so the builder can accept it
    
    comment: lots of abstraction here. It really is the guts of Backend Security.

--- backend/java-backend/src/main/java/org/example/backend/service/JWTService.java
+++ backend/java-backend/src/main/java/org/example/backend/service/JWTService.java
@@ -2,15 +2,42 @@ package org.example.backend.service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class JWTService {
+
+    private String secretKey;
+
+    //upon startup of Spring, this will run and generate a secret key using the generateSecretKey() method
+    public JWTService() {
+        secretKey = generateSecretKey();
+    }
+
+    //method to dynamically generate a secret key, as opposed to hardcoding one
+    public String generateSecretKey() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            System.out.println("Secret Key: " + secretKey.toString());
+            //then save the byte array as a String
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error generating secret key", e);
+        }
+    }
+
     public String generateToken(String username) {
 
         Map<String, Object> claims = new HashMap<>();
@@ -23,13 +50,16 @@ public class JWTService {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 // expiration set for 5 minutes
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))
+                //the signWith here is what generates the unique signature!
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
 
     }
 
     private Key getKey() {
-        // decoding logic and token generation logic goes here!
+        // convert the String back to a byte array, then return it as a Key object
+        byte [] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
 
     }
 }
