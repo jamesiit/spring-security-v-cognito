2e1f3cdedd4cdb3702a18dade8854724c460443e
James Motha <james.20232826@iit.ac.lk>
2026-03-23 15:49:32 +0530

    add helpful comments to OTPGeneratorService
    - added helpful comments in places

--- backend/java-backend/src/main/java/org/example/backend/service/OtpGeneratorService.java
+++ backend/java-backend/src/main/java/org/example/backend/service/OtpGeneratorService.java
@@ -8,14 +8,14 @@ import java.util.stream.Collectors;
 @Service
 public class OtpGeneratorService {
 
-    // 
+    // initializes once on startup
     private final SecureRandom secureRandom = new SecureRandom();
 
     public String generateOTP() {
 
-        return secureRandom.ints(6, 0, 10)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining());
+        return secureRandom.ints(6, 0, 10) // generate a stream of 6 integers between 0 and 10
+                .mapToObj(String::valueOf) // takes each int and converts it into a String
+                .collect(Collectors.joining()); // takes all Strings and converts it into one String
 
     }
 
