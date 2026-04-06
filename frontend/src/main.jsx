63cc898ce983c3f2d5e1cbf4ba45c0d41553e482
James Motha <james.20232826@iit.ac.lk>
2026-04-06 12:05:04 +0530

    Configure AWS Cognito
    - Imported Amplify library
    
    - Defined userPoolId and userPoolClientId

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Amplify } from "aws-amplify"
 import './index.css'
 import App from './App.jsx'
+
+//configure on app mount
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: import.meta.env.VITE_USER_POOL_ID,
+            userPoolClientId: import.meta.env.VITE_APP_CLIENT_ID,
+        }
+    }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
